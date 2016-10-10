41cf86f0dca24b2bd35960279a6f9b80eed9bf09
joemag <joemagiv@gmail.com>
2016-10-10 01:05:14 -0700

    Added audio and webcam texture

---
+++ Assets/DeviceCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeviceCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		WebCamTexture webcamTexture = new WebCamTexture();
+		Renderer renderer = GetComponent<Renderer>();
+		renderer.material.mainTexture = webcamTexture;
+		webcamTexture.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class LevelController : MonoBehaviour {
+	
+	private MusicManager musicManager;
+	
+	public AudioClip gameOverSound;
+	
+	// Use this for initialization
+	void Start () {
+		musicManager = FindObjectOfType<MusicManager>().GetComponent<MusicManager>();
+	}
+	
+
+	public void PlayAgain(){
+		SceneManager.LoadScene ("scene01");
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.GetComponent<PlayerMovement>()) {
+			musicManager.playSound(gameOverSound);
+			Invoke("GameOver", 4f);
+		}
+	}
+	
+	private void GameOver(){
+		SceneManager.LoadScene ("scene02-gameOver");
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
