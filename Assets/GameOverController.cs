fa2d8098eb3a589f71675dc1427374f31c8a3479
Joseph Magdalena <joemagiv@gmail.com>
2016-10-11 21:38:05 -0700

    Photo Booth Functional. Need to add art and XML for scorekeeping/screenshot tracking.

---
+++ Assets/GameOverController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameOverController : MonoBehaviour {
+	
+	public Text text;
+	
+	//public float textDelay;
+	public GameObject playAgainButton;
+	
+	// Use this for initialization
+	void Start () {
+		Debug.Log(Application.persistentDataPath);
+		playAgainButton.SetActive(false);
+		Invoke("CountdownReady",1f);
+		
+	}
+	
+	void CountdownReady(){
+		text.text = "Game Over!";
+		Invoke("Countdown3",1f);
+	}
+	
+	void Countdown3(){
+		text.text = "3";
+		Invoke("Countdown2",1f);
+		
+	}
+	
+	void Countdown2(){
+		text.text = "2";
+		Invoke("Countdown1",1f);
+		
+	}
+	
+	void Countdown1(){
+		text.text = "1";
+		Invoke("CountdownSmile",1f);
+		
+	}
+	
+	void CountdownSmile(){
+		text.text = "Smile!";
+		Invoke("TakePicture",1f);
+		
+	}
+	
+	public void TakePicture(){
+		text.text = "";
+		Application.CaptureScreenshot(Application.persistentDataPath + "Screenshot.png");
+		Invoke("PlayAgainActive", 2f);
+	}
+	
+	public void PlayAgainActive(){
+		playAgainButton.SetActive(true);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
